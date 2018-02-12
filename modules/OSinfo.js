8a12462998c31ebda9423c6e12d730b99e956f31
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-12 01:37:08 +0100

    Add files

---
+++ modules/OSinfo.js
@@ -0,0 +1,38 @@
+var os = require('os');
+var time = require('./time');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+
+    var release = os.release(); 
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();    
+    var userInfo = os.userInfo();  
+
+    console.log('System:', type);    
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);   
+    // console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');   
+    console.log('Uptime:', time.print(uptime));
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+
+exports.print = getOSinfo;
+
+/*
+exports.print = getOSinfo;
+exports.getCPUDetails = getCPUDetails;
+*/
+// alternative
+/*
+module.exports = {
+    print: getOSinfo,
+    getCPUDetails: getCPUDetails
+};
+*/
